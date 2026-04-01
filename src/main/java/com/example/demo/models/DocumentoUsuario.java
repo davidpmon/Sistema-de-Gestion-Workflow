015cb079c06e3ce313f48519ca7f0ec5e745a3ce
David Portilla <davidportillajm@hotmail.com>
2026-03-31 22:52:20 -0500

    Ajustes sincronizacion del compilador usando -parameters y reactivacion de Lombok

--- src/main/java/com/example/demo/models/DocumentoUsuario.java
+++ src/main/java/com/example/demo/models/DocumentoUsuario.java
@@ -14,19 +14,19 @@ import lombok.Setter;
 @Entity
 @Table(name = "documentos_usuario")
 public class DocumentoUsuario {
+    /*@ManyToOne define la cardinalidad muchos registros de DocumentoUsuario pueden pertenecer a un registro de Usuario*/
     @EmbeddedId
     private DocumentoUsuarioId id;
-    @ManyToOne
-    @MapsId
-    @JoinColumn(name = "id_documento")
-    private Documento documento;
-    @ManyToOne
-    @MapsId
-    @JoinColumn(name = "id_usuario")
-    private Usuario usuario;
-
     @Enumerated(EnumType.STRING)
     @Column(name = "rol_documento", nullable = false)
     private RolDocumento rolDocumento;
-
+    @ManyToOne(fetch = FetchType.LAZY)//ahorrador de carga, carga perezosa
+    @MapsId("idDocumento")//inidicador a JPA que no es una columna nueva, es una columna que se integro de la clase DocumentoUsuarioID
+    @JoinColumn(name = "id_documento", insertable = false, updatable = false)//direccion exacta del vinclo
+    private Documento documento;
+    @ManyToOne(fetch = FetchType.LAZY)//ahorrador de carga de datos
+    @MapsId("idUsuario")//inidicador a JPA que no es una columna nueva, es una columna que se integro de la clase DocumentoUsuarioID
+    //utilizado para insertar el id dentro de la llave compuesta
+    @JoinColumn(name = "id_usuario", insertable = false, updatable = false)// direccion exacta del vinculo
+    private Usuario usuario;
 }
--- src/main/java/com/example/demo/repositories/DocumentoUsuarioRepository.java
+++ src/main/java/com/example/demo/repositories/DocumentoUsuarioRepository.java
@@ -4,6 +4,8 @@ import com.example.demo.models.DocumentoUsuario;
 import com.example.demo.models.DocumentoUsuarioId;
 import com.example.demo.models.RolDocumento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,6 +13,7 @@ import java.util.Optional;
 
 @Repository
 public interface DocumentoUsuarioRepository extends JpaRepository<DocumentoUsuario, DocumentoUsuarioId> {
+
     //buscar todos los documentos de un usuario en especifico
     List<DocumentoUsuario> findById_IdUsuario(Long idUsuario);
 
